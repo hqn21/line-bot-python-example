7f54609fce149a17216422123413e988d0b1bc75
劉顥權 Haoquan Liu <haoquan291@gmail.com>
2023-11-26 11:56:17 +0800

    rename app.py to echo.py and add two kinds of generative ai bot using PaLM API

---
+++ text.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, abort
+
+from linebot.v3 import (
+    WebhookHandler
+)
+from linebot.v3.exceptions import (
+    InvalidSignatureError
+)
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+import google.generativeai as palm
+import config
+
+palm.configure(api_key=config.google_generativeai_token)
+
+app = Flask(__name__)
+
+configuration = Configuration(access_token=config.access_token)
+handler = WebhookHandler(config.channel_secret)
+
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        prompt = event.message.text
+        completion = palm.generate_text(
+            model=model,
+            prompt=prompt,
+            temperature=0,
+            # The maximum length of the response
+            max_output_tokens=800,
+        )
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=str(completion.result))]
+            )
+        )
+
+if __name__ == "__main__":
+    app.run(port=config.port, debug=True)
